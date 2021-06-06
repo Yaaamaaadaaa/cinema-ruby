a8b393e31d5d061ac596b8c9fd8b32b164ce7b97
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-06-06 19:18:34 +0900

    Movieテーブルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+[
+  ['シン・エヴァンゲリオン劇場版', 'さらば、全てのエヴァンゲリオン', '2:10', ],
+  ['ラーヤと龍の王国(吹替)', 'いま、世界の運命は彼女の手に', '2:10'],
+  ['ミッドナイトスワン', '世界で一番美しいラブストーリー', '2:10'],
+  ['砕け散るところを見せてあげる', '常識を覆す、衝撃の愛の物語', '2:10'],
+  ['映画 モンスターハンター', '想像を絶する、世界へ', '2:10'],
+  ['劇場版「鬼滅の刃」無限列車編', '果てなく続く 無限の夢の中へ', '2:10']
+].each do |title, detail, running_time|
+  Movie.create!(
+    { title: title, detail: detail, running_time: running_time}
+  )
+end
