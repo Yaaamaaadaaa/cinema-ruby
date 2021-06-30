7c1627e24e3858f67cfdbb29dde3f205c1d01d68
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-06-30 16:35:38 +0900

    上映映画の追加画面作成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,22 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.includes(:schedules)
+    @movies = Movie.includes(:schedules).order(id: "DESC")
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to root_path, notice: "映画を追加しました"
+    end
+  end
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:title, :detail, :running_time)
+    end
 end
