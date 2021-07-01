37ff56e0e63f5b957058d5237a26176287c6474b
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-07-01 16:08:47 +0900

    記述ミスの修正

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,39 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe Reservation do
+RSpec.describe User do
 
-  let(:movie) { FactoryBot.create(:movie) }
   let(:user) { FactoryBot.create(:user) }
-  let(:schedule) { FactoryBot.create(:schedule) }
 
-  describe 'new' do
+  describe 'edit' do
 
     context 'ユーザーがログインしている場合' do
 
       before do
         Movie.create!(title: 'テスト', detail: 'テスト', running_time: '02:30')
         Schedule.create!(movie_id: 1, theater_number: 1, screening_date: '9:30')
+        Reservation.create!(schedule_id: 1, user_id: user.id, reserved_seat: 'A2')
         visit new_user_session_path
         fill_in 'user[email]', with: user.email
         fill_in 'user[password]', with: user.password
         click_button 'ログイン'
-        visit new_reservation_path(schedule: 1, selects: 'A2')
+        visit users_path
       end
 
-      it '新規予約画面へ遷移できる' do
-        expect(current_path).to eq('/reservations/new')
+      it '会員情報画面へ遷移できる' do
+        expect(current_path).to eq('/users')
       end
 
       it 'お客様情報が表示される' do
         expect(page).to have_content user.email
       end
 
-      it '予約作品名が表示される' do
-        expect(page).to have_content 'テスト'
+      it '会員情報変更ボタンが表示される' do
+        expect(page).to have_button '変更はこちらから'
       end
 
-      it '上映開始時間が表示される' do
-        expect(page).to have_content '9:30'
+      it '予約済み作品が表示される' do
+        expect(page).to have_css '.card-title'
       end
 
     end
@@ -41,7 +40,7 @@ RSpec.describe Reservation do
     context 'ユーザーがログインしていない場合' do
 
       before do
-        visit new_reservation_path(schedule: 1, selects: 'A2')
+        visit users_path
       end
 
       it 'ログイン画面へ遷移する' do
