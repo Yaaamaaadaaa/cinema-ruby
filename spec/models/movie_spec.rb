d30ca9e4f1e48680ffbbe12af8e850a81496ea89
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-07-01 12:58:35 +0900

    movieモデルのテストを追加

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it "タイトル、詳細、上映時間があれば登録できる" do 
+    expect(FactoryBot.create(:movie)).to be_valid
+  end 
+
+  it "タイトルがなければ登録できない" do 
+    expect(FactoryBot.build(:movie, title: "")).to_not be_valid 
+  end 
+
+  it "タイトルが重複していたら登録できない" do 
+    movie1 = FactoryBot.create(:movie, title: "movie")
+    expect(FactoryBot.build(:movie, title: movie1.title)).to_not be_valid
+  end 
+
+  it "詳細がなければ登録できない" do 
+    expect(FactoryBot.build(:movie, detail: "")).to_not be_valid 
+  end 
+
+  it "上映時間がなければ登録できない" do 
+    expect(FactoryBot.build(:movie, running_time: "")).to_not be_valid 
+  end 
+
+end
