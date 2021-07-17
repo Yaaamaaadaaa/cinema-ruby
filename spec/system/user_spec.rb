63a79e19c3b268bfd289160b482f37e247e7bb38
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-07-17 12:31:35 +0900

    テンプレートの更新に伴うテストの更新

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User do
 
-  let(:user) { FactoryBot.create(:user) }
+  let(:admin) { FactoryBot.create(:user, id: 1) }
+  let(:user1) { FactoryBot.create(:user, id: 2) }
+  let(:user2) { FactoryBot.create(:user, id: 3) }
 
   describe 'index' do
 
@@ -11,10 +13,10 @@ RSpec.describe User do
       before do
         Movie.create!(title: 'テスト', detail: 'テスト', running_time: '02:30')
         Schedule.create!(movie_id: 1, theater_number: 1, screening_date: '9:30')
-        Reservation.create!(schedule_id: 1, user_id: user.id, reserved_seat: 'A2')
+        Reservation.create!(schedule_id: 1, user_id: user2.id, reserved_seat: 'A2')
         visit new_user_session_path
-        fill_in 'user[email]', with: user.email
-        fill_in 'user[password]', with: user.password
+        fill_in 'user[email]', with: user2.email
+        fill_in 'user[password]', with: user2.password
         click_button 'ログイン'
         visit users_path
       end
@@ -24,11 +26,11 @@ RSpec.describe User do
       end
 
       it 'お客様情報が表示される' do
-        expect(page).to have_content user.email
+        expect(page).to have_content user2.email
       end
 
       it '会員情報変更ボタンが表示される' do
-        expect(page).to have_button '変更はこちらから'
+        expect(page).to have_link '変更はこちらから'
       end
 
       it '予約済み作品が表示される' do
