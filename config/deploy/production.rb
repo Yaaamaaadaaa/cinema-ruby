57dfcd81a793e412a5f409470392ba80d6a0f46a
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-07-19 13:11:26 +0900

    デプロイ設定を追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '00.00.000.000', user: 'yamada', roles: %w{app db web} 
+server '54.150.44.117 ', user: 'yamada', roles: %w{app db web} 
 
 
 
