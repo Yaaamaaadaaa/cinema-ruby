37adc6be81e240e06a645f4e0f6bb13f59dcf42e
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-07-22 12:16:53 +0900

    過去の予約済み作品は表示されないように更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   
   def index
-    @reservations = current_user.reservations.order(id: "DESC")
+    @reservations = current_user.reservations.joins(:schedule).where("screening_date > ?",Date.today).order(id: "DESC")
   end
 end
